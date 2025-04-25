bc80ad4e66ca1d8f54866d4ecea8316227e1ff43
keyur786shah <keyur786shah@gmail.com>
2025-04-25 08:07:21 +0530

    Structural Design Pattern - BridgePattern

---
+++ StructuralDesignPattern/BridgePattern/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// Color Implementation
+public interface IColor { string Fill(); }
+
+public class Red : IColor { public string Fill() => "Red"; }
+public class Blue : IColor { public string Fill() => "Blue"; }
+
+// Shape Abstraction
+public abstract class Shape
+{
+    protected IColor _color;
+    protected Shape(IColor color) { _color = color; }
+    public abstract void Draw();
+}
+
+public class Circle : Shape
+{
+    public Circle(IColor color) : base(color) { }
+    public override void Draw() => Console.WriteLine($"Drawing Circle in {_color.Fill()}");
+}
+
+public class Square : Shape
+{
+    public Square(IColor color) : base(color) { }
+    public override void Draw() => Console.WriteLine($"Drawing Square in {_color.Fill()}");
+}
+
+// Client
+class Program
+{
+    static void Main()
+    {
+        Shape redCircle = new Circle(new Red());
+        Shape blueSquare = new Square(new Blue());
+
+        redCircle.Draw();   // Drawing Circle in Red
+        blueSquare.Draw();  // Drawing Square in Blue
+    }
+}
+
+
